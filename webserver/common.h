5b6cc1dbb71fec12df9dde39f0af9acea2a23cb4
mengpl <mengpeiliang@gmail.com>
2017-04-13 14:15:07 +0800

    add

---
+++ webserver/common.h
@@ -0,0 +1,22 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+namespace web_server
+{
+
+    static inline uint32_t simple_hash(const char *name) 
+    {
+        unsigned char *s = (unsigned char *) name;
+        uint32_t hval = 0x811c9dc5;
+        while (*s) {
+            hval *= 16777619;
+            hval ^= (uint32_t) *s++;
+        }
+        return hval;
+    };
+
+    
+
+}
+
+#endif // __COMMON_H__
